0b2c1d887902b27ad7e6a6ca26541200b630cc4f
Christophe Faivre <christophe@faivre.co.za>
2011-10-02 22:35:19 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ItmAdmin::Application.config.secret_token = '1551714052f8c97290af6576e3f37fa4bce36edc342be0b86cac71e9f2d0f49a62af4d6cf303b52f126fb8a616033d4fd5febf7498a120e7625a705590e13df1'
